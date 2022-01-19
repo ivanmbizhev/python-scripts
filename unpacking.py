edfe51252d9bc111d9d644e61d48c5b1bdea8ae1
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-01-19 12:07:09 +0200

    a

---
+++ unpacking.py
@@ -0,0 +1,16 @@
+a = b = c = d = e = f = 12
+print(c)
+
+x, y, z = 1, 2, 76
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a tuple")
+
+data_list = [12, 13, 14]
+data_list.append(15)
+p, q, r = data_list
+print(p)
+print(q)
+print(r)
