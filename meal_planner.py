0529acce6f21bae4f20d0639fd575478afbe9ea8
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-01-28 17:51:04 +0200

    m

--- meal_planner.py
+++ meal_planner.py
@@ -1,4 +1,21 @@
 from contents import pantry, recipes
 
-print(pantry)
-print(recipes)
+# display_dict = {str(index + 1): meal for index, meal in enumerate(recipes)}
+display_dict = {}
+for index, key in enumerate(recipes):
+    display_dict[str(index + 1)] = key
+
+while True:
+    # Display a menu of the recepes we know how to cook
+    print("Please choose your recipe")
+    print("-------------------------")
+    for key, value in display_dict.items():
+        print(f"{key} - {value}")
+
+    choice = input(": ")
+
+    if choice == "0":
+        break
+    elif choice in display_dict:
+        selected_item = display_dict[choice]
+        print(f"You haver selected {selected_item}")
