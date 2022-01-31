04ed8b5adf02222b368ff627a9b7d9d32d0b1dd4
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-01-31 11:28:16 +0200

    l

--- meal_planner.py
+++ meal_planner.py
@@ -1,3 +1,4 @@
+from os import initgroups
 from contents import pantry, recipes
 
 # display_dict = {str(index + 1): meal for index, meal in enumerate(recipes)}
@@ -12,10 +13,20 @@ while True:
     for key, value in display_dict.items():
         print(f"{key} - {value}")
 
-    choice = input(": ")
+    choice = input("> ")
 
     if choice == "0":
         break
     elif choice in display_dict:
         selected_item = display_dict[choice]
         print(f"You haver selected {selected_item}")
+        print("checking ingredients...")
+        ingredients = recipes[selected_item]
+        print(ingredients)
+        for food_item, required_quantity in ingredients.items():
+            quantity_in_pantry = pantry.get(food_item, 0)
+            if required_quantity <= quantity_in_pantry:
+                print(f"\t{food_item} OK")
+            else:
+                quantity_in_pantry = required_quantity - quantity_in_pantry
+                print(f"\tYou need to buy {quantity_in_pantry} of {food_item}")
