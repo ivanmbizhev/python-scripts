caabcd3e7ffde654e7e6edcf1d5f462ba62187f1
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-02-01 14:53:28 +0200

    l

--- git_commit.sh
+++ git_commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter the file to add: "
-read punkt
+read  punkt
 echo "Please enter your commit: "
-read commit
+read  commit
 
 
 git add $punkt && git commit -m "$commit"
