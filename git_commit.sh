1dfa5a37c0238f2817069dd2900ee134b468f861
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-02-01 14:57:24 +0200

    a

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script is meant only for a local system and can be used to add and commit your changes to the repo. 
+# Made by Ibizhev
+
 echo "Current status: "
 echo ------------------------------------------
 git status
