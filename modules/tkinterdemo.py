4ca4d64d9ee786c6b1ed850b44c18825ff4df6f0
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-03-01 14:23:42 +0200

    1

--- modules/tkinterdemo.py
+++ modules/tkinterdemo.py
@@ -1,21 +1,41 @@
+import re
 import tkinter
 
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello there")
-mainWindow.geometry('640x480+8+400')
+mainWindow.geometry('640x480+8-200')
 
 label = tkinter.Label(mainWindow, text="Hello there")
-label.pack(side='top')
+label.grid(row=0, column=0)
 
-canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
-canvas.pack(side='left')
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.grid(row=1, column=1)
 
-button1 = tkinter.Button(mainWindow, text="button1")
-button2 = tkinter.Button(mainWindow, text="button2")
-button3 = tkinter.Button(mainWindow, text="button3")
-button1.pack(side='left', anchor='n')
-button2.pack(side='left', anchor='s')
-button3.pack(side='left', anchor='e')
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.grid(row=1, column=0)
 
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.grid(row=1, column=2, sticky='n')
+
+button1 = tkinter.Button(rightFrame, text="button1")
+button2 = tkinter.Button(rightFrame, text="button2")
+button3 = tkinter.Button(rightFrame, text="button3")
+button1.grid(row=0, column=0)
+button2.grid(row=1, column=0)
+button3.grid(row=2, column=0)
+
+#Configure the columns
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1)
+
+leftFrame.config(relief='sunken', borderwidth=1)
+rightFrame.config(relief='sunken', borderwidth=1)
+leftFrame.grid(sticky='ns')
+rightFrame.grid(sticky='new')
+
+rightFrame.columnconfigure(0, weight=1)
+button2.grid(sticky='ew')
 mainWindow.mainloop()
+
