a76a4c9a35678224aef01b52ae545a3940d1a760
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-03-14 12:59:04 +0200

    a

--- modules/functions.py
+++ modules/functions.py
@@ -1,13 +1,25 @@
-from cgitb import text
-from turtle import left, width
-
-
 def python_food():
     width = 80
     text = "Spam and eggs"
     left_margin = (width - len(text)) // 2
     print(" " * left_margin, text)
 
-# Call the function
+def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
+    left_margin = (80 - len(text)) // 2
+    return " " * left_margin + text
 
-python_food()
+#with open("centred", mode='w') as centered_file:
+
+# Call the function
+print(centre_text("spam and eggs"))
+print(centre_text("spam, spam and eggs"))
+print(centre_text(12))
+print(centre_text("spam, spam, spam and spam"))
+
+print(centre_text("first", "second", 3, 4, "spam", sep=':'))
+
+print("=" + str(12*3))
+print(sorted(["b", "d", "c", "a"]))
