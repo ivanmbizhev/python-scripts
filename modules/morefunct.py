a819e732547f7d260a86ee6afcb80a6779a658ed
Ivan Bizhev <ivanmbizhev@gmail.com>
2022-03-18 11:49:22 +0200

    1

--- modules/morefunct.py
+++ modules/morefunct.py
@@ -1,8 +1,21 @@
+import math
 import tkinter as tkinter
 
-def parabola(x):
-    y = x * x / 100
-    return y
+def parabola(page, size):
+    for x in range(-size, size):
+        y = x * x / size
+        plot(page, x, y)
+        plot(page, -x, y)
+
+def circle(page, radius, g, h):
+    page.create_oval(g + radius, -(h - radius), g - radius, -(h + radius), outline="red", width=2)
+    # for x in range(g * 100, (g + radius) * 100):
+    #     x /= 100
+    #     y = h + (math.sqrt(radius ** 2 - ((x-g) ** 2)))
+    #     plot(page, x, y)
+    #     plot(page, x, 2 * h - y)
+    #     plot(page, 2 * g - x, y)
+    #     plot(page, 2 * g - x, 2 * h - y)
 
 def draw_axes(canvas):
     canvas.update()
@@ -11,9 +24,10 @@ def draw_axes(canvas):
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
     canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
     canvas.create_line(0, -y_origin, 0, y_origin, fill="black")
+    print(locals())
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill="red")
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill="red")
 
 mainWindow = tkinter.Tk()
 
@@ -23,10 +37,23 @@ mainWindow.geometry("640x480")
 canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
-draw_axes(canvas)
+# canvas2 = tkinter.Canvas(mainWindow, width=640, height=480, background="blue")
+# canvas2.grid(row=0, column=1)
 
-for x in range(-100, 100):
-    y = parabola(x)
-    plot(canvas, x, -y)
+draw_axes(canvas)
+# draw_axes(canvas2)
+
+parabola(canvas, 100)
+parabola(canvas, 200)
+circle(canvas, 100, 100, 100)
+circle(canvas, 100, 100, -100)
+circle(canvas, 100, -100, 100)
+circle(canvas, 100, -100, -100)
+circle(canvas, 10, 30, 30)
+circle(canvas, 10, 30, -30)
+circle(canvas, 10, -30, 30)
+circle(canvas, 10, -30, -30)
+circle(canvas, 30, 0, 0)
+
 
 mainWindow.mainloop()
